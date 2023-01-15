f1af4f4c573bd3acc09b84118a11b974f4c2962a
Kyrychenko-90 <makedonski.ua@gmail.com>
2023-01-15 20:41:19 +0200

    update task_5

--- task_5/js/script.js
+++ task_5/js/script.js
@@ -2,8 +2,8 @@
 //     отримати від користувача через три prompt три числа
 //     показати через alert середнє арифметичне цих чисел
 
-const numberOne = prompt('Введіть перше число');
-const numberTwo = prompt('Введіть друге число');
-const numberThree = prompt('Введіть третє число');
-const average = (+numberOne + +numberTwo + +numberThree) / 3;
+const numberOne = +prompt('Введіть перше число');
+const numberTwo = +prompt('Введіть друге число');
+const numberThree = +prompt('Введіть третє число');
+const average = (numberOne + numberTwo + numberThree) / 3;
 alert(`Середнє арифметичне цих чисел дорівнює ${average}`);
