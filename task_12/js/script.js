07e603d8d3d0a99568b7b7c6cb293dc12ab31de4
Kyrychenko-90 <makedonski.ua@gmail.com>
2023-01-25 12:29:04 +0200

    add task_12

--- task_12/js/script.js
+++ task_12/js/script.js
@@ -1,2 +1,17 @@
+//  Створити масив, довжину та елементи якого задає користувач.
+//  Відсортувати масив за зростанням.
+//  Видалити елементи з масиву з 2 по 4 (включно!).
+//  У міру змін виводити вміст масиву на сторінку.
 
+const myAray = [4, 14, 12, 27, 2, 37, 8];
+console.log(myAray);
 
+function itemsSort (a, b) {
+    return a - b ;
+}
+
+myAray.sort(itemsSort)
+console.log(myAray);
+
+myAray.splice(1,3);
+console.log(myAray);
