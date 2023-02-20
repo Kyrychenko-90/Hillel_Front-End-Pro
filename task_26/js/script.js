5c9f5678a2d8f3fd10b8c232b2f03fddd7db1206
Kyrychenko-90 <makedonski.ua@gmail.com>
2023-02-20 21:15:49 +0200

    up task_26

--- task_26/js/script.js
+++ task_26/js/script.js
@@ -51,14 +51,10 @@ class Car {
 }
 
 const human1 = new Human("Bob", 25);
-console.log(human1);
 const human2 = new Human("Eva", 23);
-console.log(human2);
 
 const car1 = new Car("Porsche", "Panamera", 2021, "CB7777AX");
-console.log(car1);
 const car2 = new Car("Lamborghini", "Aventador", 2020, "BM5555AX");
-console.log(car2);
 
 car1.addOwner(human1);
 car2.addOwner(human2);
