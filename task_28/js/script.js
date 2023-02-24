9e806ee836d8b83534319fc9209bc80ca1eddbe4
Kyrychenko-90 <makedonski.ua@gmail.com>
2023-02-24 14:22:44 +0200

    update task_28

--- task_28/js/script.js
+++ task_28/js/script.js
@@ -1,12 +1,12 @@
 //  1.Создать функцию которая будет принимать миллисекунды и вернет true или false
 //      в зависимости от того сегодня ли то число которое было передано
 
-const today = (mSec1) => {
+const checkDate = (mSec1) => {
     const yourDate = new Date(mSec1);
     const today = new Date();
     return yourDate.getDay() === today.getDay() && yourDate.getMonth() === today.getMonth() && yourDate.getFullYear() === today.getFullYear();
 }
-console.log(today(1677231208631));
+console.log(checkDate(1677231208631));
 
 //  2.Создать функцию которая вернет дату и время в формате 13.10.2022 13:13 принимая миллисекунды
 
