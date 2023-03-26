c909b01568badfe8ab84c59f2e5254dec08f296d
Kyrychenko-90 <makedonski.ua@gmail.com>
2023-03-27 00:45:18 +0300

    add task_37

---
+++ task_37/js/script.js
@@ -0,0 +1,15 @@
+const btm = document.querySelectorAll(".btm");
+
+btm.forEach(button => {
+    button.addEventListener("click", event => {
+        const href = button.getAttribute("data-href");
+        if (!href.includes("http://") && !href.includes("https://")) {
+            location.href = `https://${href}`;
+        } else {
+            location.href = href;
+        }
+    });
+});
+
+
+
